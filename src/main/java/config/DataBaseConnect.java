23d05716e0771fee495bb6e19ce9866538901593
marciondemelo <marciondemelo@gmail.com>
2022-03-30 16:39:01 -0300

    ajuste na classe databaseconnect

--- src/main/java/config/DataBaseConnect.java
+++ src/main/java/config/DataBaseConnect.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.util.Locale;
+
 @Configuration
 public class DataBaseConnect {
     private final CredentialDataBase credentialDataBase;
@@ -17,9 +19,18 @@ public class DataBaseConnect {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(credentialDataBase.getClassName());
-        dataSource.setUrl(credentialDataBase.getUrl());
-        return dataSource;
+        if(credentialDataBase.getClassName().toLowerCase(Locale.ROOT).contains("h2")){
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName(credentialDataBase.getClassName());
+            dataSource.setUrl(credentialDataBase.getUrl());
+            return dataSource;
+        }else{
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName(credentialDataBase.getClassName());
+            dataSource.setUrl(credentialDataBase.getUrl());
+            dataSource.setUsername(credentialDataBase.getUserName());
+            dataSource.setPassword(credentialDataBase.getPassword());
+            return dataSource;
+        }
     }
 }
